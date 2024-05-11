aa5d36e0fce73a64f93f2535cdfbcbfdc85444c3
Aayush Pargal <aayushpargal987@gmail.com>
2024-05-11 14:31:02 +0530

    allowed options

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+// app.use(cors());
+const allowedOrigins = [
+  "https://file-management-sooty.vercel.app",
+  "https://file-management-soott.vercel.app",
+  "http://localhost:5173",
+];
 
-const userRouter = require("./Routes/UserRoute")
-const FileRoute = require("./Routes/FileRoute")
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+const userRouter = require("./Routes/UserRoute");
+const FileRoute = require("./Routes/FileRoute");
 const PORT = 3000;
 
 mongoose
@@ -18,10 +36,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-
 app.use("/user", userRouter);
 app.use("/dashboard", FileRoute);
-
-
 
 app.listen(PORT, () => console.log("Connected to port 3000"));
